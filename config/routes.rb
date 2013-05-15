f4c4020006f344e596f9eaf7f4006724c3d56cfd
Hanna Seyoum <seyoumha@gmail.com>
2013-05-15 16:56:33 -0400

    cumtom routes for auth and feed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Treebook::Application.routes.draw do
   devise_for :users
 
+  devise_scope :user do
+    get 'register', to: 'devise/registrations#new', as: :register
+    get 'login', to: 'devise/sessions#new', as: :login
+    get 'logout', to: 'devise/sessions#destroy', as: :logout
+  end
+
+
+
   resources :statuses
+  get "feed", to: 'statuses#index', as: :feed
 
 
   # The priority is based upon order of creation:
